c8e91af3dda4d42648ed292661cea95ef1ab0d81
Brian <brianfu9@gmail.com>
2018-11-03 13:21:56 -0700

    form linked

--- serve.py
+++ serve.py
@@ -1,22 +1,45 @@
 # serve.py
 
-from flask import Flask  
-from flask import render_template
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 from google.cloud import vision
 
 # creates a Flask application, named app
 app = Flask(__name__)
 app._static_folder = './static'
+app.config['SECRET_KEY'] = 'briant'
+
+URI = ''
+
+class ReusableForm(Form):
+    name = TextField('Name:', validators=[validators.required()])
 
 # a route where we will display a welcome message via an HTML template
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
     message = "Hello, World"
-    return render_template('index.html', message=message)
+    form = ReusableForm(request.form)
+    print("errors: ", form.errors)
+    if request.method == 'POST':
+        url = request.form['name']
+        print("url: ", url) 
+        if form.validate():
+            # Save the comment here.
+            URI = url
+            flash(url)
+        else:
+            flash('Invalid URL')
+    return render_template('index.html', message=message, form = form)
+
+
+@app.route("/result")
+def result():
+    message = "Hello, World"
+    return render_template('result.html', message=message)
 
 
 @app.route("/test")
-def test():  
+def test():
     message = "Hello, World"
     return render_template('test.html', message=message)
 
